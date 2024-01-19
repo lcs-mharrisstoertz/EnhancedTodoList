3ccd418da5149b9801de8ed37db4195ca418c01b
Morgan Harris-Stoertz <mharrisstoertz@lcs.on.ca>
2024-01-19 14:09:44 -0500

    Initial Commit

---
+++ EnhancedTodoList/EnhancedTodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  EnhancedTodoListApp.swift
+//  EnhancedTodoList
+//
+//  Created by Morgan Harris-Stoertz on 2024-01-19.
+//
+
+import SwiftUI
+
+@main
+struct EnhancedTodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
