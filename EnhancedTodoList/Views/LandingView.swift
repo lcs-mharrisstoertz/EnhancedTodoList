0377847399fe40949a48ac43744ce44dd7df2632
Morgan Harris-Stoertz <mharrisstoertz@lcs.on.ca>
2024-01-19 14:12:34 -0500

    initial organization done

---
+++ EnhancedTodoList/Views/LandingView.swift
@@ -0,0 +1,18 @@
+//
+//  LandingView.swift
+//  EnhancedTodoList
+//
+//  Created by Morgan Harris-Stoertz on 2024-01-19.
+//
+
+import SwiftUI
+
+struct LandingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LandingView()
+}
